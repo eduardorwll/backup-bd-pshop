7d7d673aed1c33ead38a44fa73f97b1607613343
eduardorwll <dudurewell@gmail.com>
2025-12-11 22:22:44 -0300

    !Adiciona script de backup v0.1.0

---
+++ backup.bat
@@ -0,0 +1,54 @@
+@echo off
+
+setlocal enabledelayedexpansion
+set LOG_FILE=backup_databases_%timestamp%.log
+
+CALL env.bat
+
+echo ======================================= >> "%LOG_FILE%"
+echo Script iniciado: %DATE% %TIME% >> "%LOG_FILE%"
+echo ======================================= >> "%LOG_FILE%"
+
+REM Contador de erros
+set /a ERROS=0
+
+for %%d in (%databases%) do (
+    echo ------------------------------------------------------- >> "%LOG_FILE%"
+    echo Iniciando backup da database: "%%d" >> "%LOG_FILE%"
+
+    sqlcmd.exe -S localhost\SQLExpress -Q "BACKUP DATABASE %%d TO DISK='C:\Temp\%%d_%timestamp%.bak' WITH FORMAT" 2>> "%LOG_FILE%"
+
+    if !ERRORLEVEL! equ 0 (
+        echo [%TIME%] SUCESSO: Backup de "%%d" concluido >> "%LOG_FILE%"
+    ) else (
+        echo [%TIME%] ERRO !ERRORLEVEL!: Falha no backup de "%%d" >> "%LOG_FILE%"
+        set /a ERROS+=1
+    )
+)
+
+for %%d in (%databases%) do (
+    echo ------------------------------------------------------- >> "%LOG_FILE%"
+    echo Iniciando compressão do backup da database: "%%d" >> "%LOG_FILE%"
+
+    "c:\Program Files (x86)\7-Zip\7z.exe" a -t7z C:\Temp\"%%d_%timestamp%".7z C:\Temp\"%%d_%timestamp%".bak
+
+    if !ERRORLEVEL! equ 0 (
+        echo [%TIME%] SUCESSO: Compressão do backup de "%%d" concluido >> "%LOG_FILE%"
+
+        del C:\Temp\"%%d_%timestamp%".bak
+    ) else (
+        echo [%TIME%] ERRO !ERRORLEVEL!: Falha na compressão do backup de "%%d" >> "%LOG_FILE%"
+        set /a ERROS+=1
+    )
+)
+
+if %ERROS% neq 0 (
+    echo ======================================= >> "%LOG_FILE%"
+    echo Script rodou com erros. Apagando arquivos .7z e .bak >> "%LOG_FILE%"
+    echo ======================================= >> "%LOG_FILE%"
+
+    for %%d in (%databases%) do (
+        del C:\Temp\"%%d_%timestamp%".7z 
+        del C:\Temp\"%%d_%timestamp%".bak
+    )
+)
